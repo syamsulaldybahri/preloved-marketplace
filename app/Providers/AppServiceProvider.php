063787356f17b2b7eff1be38c8b677dd1dfd6b0e
Syamsul Aldy Bahri <syamsulaldybahri@gmail.com>
2026-01-04 19:41:00 +0800

    fix tampilan raksasa dan force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Tambahkan kode ini agar semua link aset menggunakan HTTPS
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
